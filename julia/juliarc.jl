d8981a866f83607db16cca11ceebe4899ffac85c
Josh Langsfeld <jdlangs@gmail.com>
2015-06-25 12:45:55 -0400

    updates

--- julia/juliarc.jl
+++ julia/juliarc.jl
@@ -9,3 +9,23 @@ macro run(file)
         end
     end
 end
+
+function fwhos(m::Module, pattern::Regex; filter=[])
+    filtertypes = applicable(start, filter) ?
+        filter : [filter]
+    for ft in filtertypes
+        isa(ft, DataType) ||
+        throw(ArgumentError("Non-type object given to filter keyword"))
+    end
+    filtertest(var) = ! any(T -> typeof(var) <: T, filtertypes)
+    for n in sort(names(m))
+        s = string(n)
+        v = eval(m,n)
+        if isdefined(m,n) && ismatch(pattern, s) && filtertest(v)
+            println(rpad(s, 30), summary(v))
+        end
+    end
+end
+fwhos(;filter=Module) = fwhos(r""; filter=filter)
+fwhos(m::Module; filter=[]) = fwhos(m, r""; filter=filter)
+fwhos(pat::Regex; filter=Module) = fwhos(current_module(), pat; filter=filter)
