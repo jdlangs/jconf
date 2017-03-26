2e9b131579d946c6719bb1163f7b910ebd53242f
Josh Langsfeld <jdlangs@gmail.com>
2017-03-26 14:24:59 -0700

    fish ctrl-f issue

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,10 @@ setxkbmap -option ctrl:nocaps
 set -x JULIA_SHELL /bin/bash
 set -x JULIA_PKGDIR ~/Tools/julia-packages
 fish_vi_key_bindings
+
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
+fish_user_key_bindings
